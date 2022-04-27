f9db23453286b3e08a948eb02961a14ed3c5c47a
alsino <iamahorse@gmail.com>
2022-04-27 11:29:47 +0200

    updated headline script

--- updateHeadline.js
+++ updateHeadline.js
@@ -28,9 +28,7 @@ const googleClient = new Translate({ key: process.env.API_KEY });
 const response = await fetch('https://data2.unhcr.org/population/?widget_id=294522&sv_id=54&population_group=5460');
 const res = await response.json();
 const totalRefugees = await res.data[0].individuals;
-// const headingLatest = `More than ${toFixed(totalRefugees / 1000000, 1)} million refugees have already fled from Ukraine`;
-
-const headingLatest = `More than 000 million refugees have already fled from Ukraine`;
+const headingLatest = `More than ${toFixed(totalRefugees / 1000000, 1)} million refugees have already fled from Ukraine`;
 
 console.log(chalk.green(`New headline: ${headingLatest}` ));
 
